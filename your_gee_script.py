d840c72ec80247c538fdd17696e45cb09c49d45f
Nived Padikkal <97820044+nivutechy@users.noreply.github.com>
2023-07-28 05:13:43 -0700

    Update your_gee_script.py

--- your_gee_script.py
+++ your_gee_script.py
@@ -1,46 +1,47 @@
 import ee
 
-# Initialize the Earth Engine API
+# Initialize Earth Engine
 ee.Initialize()
 
-def export_satellite_image(bbox, output_filename):
-    # Convert the bounding box coordinates to a geometry object
-    bbox_geometry = ee.Geometry.Rectangle(bbox)
+# Function to get satellite imagery
+def get_satellite_image(bbox=[longitude_west_default, latitude_south_default, longitude_east_default, latitude_north_default]):
+    # Define the bounding box
+    region = ee.Geometry.Rectangle(bbox)
 
-    # Load Landsat 8 imagery collection
-    landsat = ee.ImageCollection('LANDSAT/LC08/C01/T1')
+    # Get a satellite image (e.g., Landsat 8 imagery)
+    image = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA') \
+        .filterBounds(region) \
+        .filterDate('2020-01-01', '2023-02-01') \
+        .first()
 
-    # Filter the imagery collection by the bounding box and date range (if needed)
-    # Example date range filter:
-    # landsat = landsat.filterDate('YYYY-MM-DD', 'YYYY-MM-DD')
+    # Export the image to Google Drive
+    task = ee.batch.Export.image.toDrive(image=image,
+                                         description='satellite_image',
+                                         folder='GEE_Export',
+                                         fileNamePrefix='exported_image',
+                                         region=region.getInfo(),
+                                         scale=30)
 
-    # Filter the imagery collection based on the bounding box
-    landsat = landsat.filterBounds(bbox_geometry)
-
-    # Select the latest image from the filtered collection (or any other specific image selection criteria)
-    image = ee.Image(landsat.sort('system:time_start', False).first())
-
-    # Select the bands you want to export
-    bands = ['B4', 'B3', 'B2']  # Example: Red, Green, and Blue bands
-
-    # Clip the image to the bounding box
-    clipped_image = image.clip(bbox_geometry)
-
-    # Export the image as a GeoTIFF file
-    task = ee.batch.Export.image.toDrive(
-        image=clipped_image,
-        description='Satellite_Image_Export',
-        folder='Satellite_Images',  # The folder where the exported image will be saved in Google Drive
-        fileNamePrefix=output_filename,
-        region=bbox_geometry,
-        scale=30,  # The spatial resolution of the exported image (30 meters for Landsat)
-    )
-
+    # Start the export task
     task.start()
 
-if __name__ == '__main__':
-    # Define the bounding box as [minX, minY, maxX, maxY]
-    bounding_box = [-122.5258, 37.6493, -122.3442, 37.8199]  # Example bounding box for San Francisco, USA
+    # Wait for the task to complete
+    print("Exporting the image to Google Drive...")
+    task_status = None
+    while task_status != 'COMPLETED':
+        task_status = ee.batch.Task.list()[0]['state']
+        if task_status == 'FAILED':
+            print("Export task failed. Please check the GEE tasks for more information.")
+            return
 
-    # Call the function to export the satellite image
-    export_satellite_image(bounding_box, 'exported_image')
+    print("Image exported successfully!")
+
+if __name__ == "__main__":
+    # Set the default bounding box coordinates [west, south, east, north]
+    longitude_west_default = -74.5
+    latitude_south_default = 40.0
+    longitude_east_default = -73.5
+    latitude_north_default = 41.0
+
+    # Call the function to get the satellite image and export it
+    get_satellite_image()
